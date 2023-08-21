e73f16e012d2b87e5122b96429274f10e36df449
John Tyner <jtyner@gmail.com>
2023-08-21 15:25:58 -0400

    add numerical conversions

--- src/ffx.rs
+++ src/ffx.rs
@@ -1,6 +1,9 @@
 use crate::error::Error;
 use crate::result::Result;
 
+use std::ops::Add;
+use std::ops::Mul;
+
 struct SizeLimits {
     min: usize,
     max: usize,
@@ -13,13 +16,9 @@ struct FFXSizeLimits {
 
 pub struct FFX {
     cipher: openssl::cipher_ctx::CipherCtx,
-
-    radix: usize,
-    alpha: Vec<char>,
-
+    twk: Vec<u8>,
     len: FFXSizeLimits,
-
-    twk: Option<Vec<u8>>,
+    alpha: Vec<char>,
 }
 
 const DEFAULT_ALPHABET: &str = "0123456789abcdefghijklmnopqrstuvwxyz";
@@ -46,6 +45,7 @@ impl FFX {
         if radix < 2 || radix > chars.len() {
             return Err(Error::new("invalid radix"));
         }
+        chars.truncate(radix);
 
         let mintxt = (6f64 / (radix as f64).log10()).ceil() as usize;
         if mintxt < 2 || mintxt > maxtxt {
@@ -58,15 +58,15 @@ impl FFX {
             ));
         }
 
-        let twk: Option<Vec<u8>>;
+        let twk: Vec<u8>;
         match opt_twk {
-            None => twk = None,
+            None => twk = Vec::new(),
             Some(t) => {
                 if t.len() < mintwk || (maxtwk > 0 && t.len() > maxtwk) {
                     return Err(Error::new("invalid tweak length"));
                 }
 
-                twk = Some(t.to_vec());
+                twk = t.to_vec();
             }
         }
 
@@ -95,9 +95,7 @@ impl FFX {
 
         Ok(FFX {
             cipher: cipher,
-
-            radix: radix,
-            alpha: chars,
+            twk: twk,
 
             len: FFXSizeLimits {
                 twk: SizeLimits {
@@ -110,8 +108,35 @@ impl FFX {
                 },
             },
 
-            twk: twk,
+            alpha: chars,
         })
+    }
+
+    pub fn get_tweak<'a>(&'a self, opt_twk: &Option<&'a [u8]>) -> &'a [u8] {
+        match opt_twk {
+            None => &self.twk,
+            Some(t) => t,
+        }
+    }
+
+    pub fn get_alphabet<'a>(&'a self) -> &'a Vec<char> {
+        &self.alpha
+    }
+
+    pub fn validate_text_length(&self, n: usize) -> Result<()> {
+        if n < self.len.txt.min || n > self.len.txt.max {
+            return Err(Error::new("invalid text length"));
+        }
+
+        Ok(())
+    }
+
+    pub fn validate_tweak_length(&self, n: usize) -> Result<()> {
+        if n < self.len.twk.min || n > self.len.twk.max {
+            return Err(Error::new("invalid tweak length"));
+        }
+
+        Ok(())
     }
 
     pub fn prf(&self, d: &mut [u8], s: &[u8]) -> Result<()> {
@@ -149,6 +174,73 @@ impl FFX {
     }
 }
 
+pub fn reverse<T: Copy>(s: &[T]) -> Vec<T> {
+    let mut d = Vec::<T>::with_capacity(s.len());
+    for i in 0..s.len() {
+        d.push(s[s.len() - i - 1]);
+    }
+    d
+}
+
+pub fn chars_to_big_num(
+    chars: &[char],
+    alpha: &[char],
+) -> Result<openssl::bn::BigNum> {
+    let mut n = openssl::bn::BigNum::from_u32(0)?;
+    let mut m = openssl::bn::BigNum::from_u32(1)?;
+    let mut t = openssl::bn::BigNum::new()?;
+
+    for c in reverse(chars) {
+        let mut idx = alpha.len();
+
+        for i in 0..alpha.len() {
+            if c == alpha[i] {
+                idx = i;
+                break;
+            }
+        }
+
+        if idx >= alpha.len() {
+            return Err(Error::new("invalid character encountered"));
+        } else if idx > 0 {
+            t.clear();
+            t.add_word(idx as u32)?;
+            t = t.mul(&m);
+            n = n.add(&t);
+        }
+
+        m.mul_word(alpha.len() as u32)?;
+    }
+
+    Ok(n)
+}
+
+pub fn big_num_to_chars(
+    mut n: openssl::bn::BigNum,
+    alpha: &[char],
+    opt_len: Option<usize>,
+) -> Result<Vec<char>> {
+    let z = openssl::bn::BigNum::from_u32(0)?;
+
+    let mut chars = Vec::<char>::new();
+
+    while n.ne(&z) {
+        let r = n.div_word(alpha.len() as u32)?;
+        chars.push(alpha[r as usize]);
+    }
+
+    if opt_len.is_some() {
+        let l = opt_len.unwrap();
+
+        while chars.len() < l {
+            chars.push(alpha[0]);
+        }
+    }
+
+    chars.reverse();
+    Ok(chars)
+}
+
 #[cfg(test)]
 mod tests {
     use super::FFX;
@@ -171,6 +263,23 @@ mod tests {
 
         assert!(d1 == d2);
         assert!(d1 == exp);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_bignum_conversion() -> Result<()> {
+        let mut alpha = Vec::<char>::new();
+        "0123456789".chars().for_each(|c| alpha.push(c));
+
+        let n_str = "9037450980398204379409345039453045723049";
+        let n = openssl::bn::BigNum::from_dec_str(n_str)?;
+
+        let c = super::big_num_to_chars(n.to_owned()?, &alpha, None)?;
+        assert!(String::from_iter(c.clone()) == n_str);
+
+        let r = super::chars_to_big_num(&c, &alpha)?;
+        assert!(n == r);
 
         Ok(())
     }
