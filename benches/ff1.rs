dd288acfef365bdc143287aaffb55059dbad9097
John Tyner <jtyner@gmail.com>
2023-08-22 08:53:32 -0400

    add context creation to bench tests

--- benches/ff1.rs
+++ benches/ff1.rs
@@ -1,5 +1,9 @@
 extern crate bencher;
 
+fn context(b: &mut bencher::Bencher) {
+    b.iter(|| fpe::ff1::FF1::new(&[0; 32], None, 0, 0, 26, None).unwrap());
+}
+
 fn encrypt(b: &mut bencher::Bencher) {
     let ff1 = fpe::ff1::FF1::new(&[0; 32], None, 0, 0, 26, None).unwrap();
     b.iter(|| ff1.encrypt("0123456789", None));
@@ -11,5 +15,5 @@ fn decrypt(b: &mut bencher::Bencher) {
     b.iter(|| ff1.decrypt(&ct, None));
 }
 
-bencher::benchmark_group!(benches, encrypt, decrypt);
+bencher::benchmark_group!(benches, context, encrypt, decrypt);
 bencher::benchmark_main!(benches);
